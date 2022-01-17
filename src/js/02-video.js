60ad1ba43cf372e50b1df8f54c85cfb1e2cb946e
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-17 15:41:24 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 // import Vimeo from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
@@ -7,34 +8,31 @@ player.on('play', function () {
     const currentTime = localStorage.getItem("videoplayer-current-time");
     
     player.setCurrentTime(currentTime);
-});
-    
-    // player.data = parsedData;
-    // console.log(player.data);
+});      
         
-    console.log('played the video!');
-    player.on('timeupdate', function (data) {
-            
-            console.log(data);  
+console.log('played the video!');
+
+
+    
+function setLocalStorageCurrentTime (data) {           
+          
         localStorage.setItem("videoplayer-current-time", data.seconds);
         console.log(data.seconds); 
-        }
-    );
+        };
+
+const throttledSetLocalStorageCurrentTime = throttle(setLocalStorageCurrentTime, 1000);  
+      
+player.on('timeupdate', throttledSetLocalStorageCurrentTime);
+
+
+function setLocalStorageCurrentTime (data) {           
+          
+        localStorage.setItem("videoplayer-current-time", data.seconds);
+        console.log(data.seconds); 
+        };
 
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
 
     
-// player.getCurrentTime().then(function (seconds) {
-//        console.log('seconds:', seconds);
-//     // seconds = the current playback position
-// }).catch(function(error) {
-//     // an error occurred
-// }); 
-// player.addEventListener('timeupdate', getEventCurrentTime);
-
-// function getEventCurrentTime(e) {
-//     console.log(e.player.)
-    
-// }
