11506c0dd96349a4557996f1aad1b42ca4817fe5
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-17 11:53:42 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+// import Vimeo from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.getCurrentTime().then(function (seconds) {
+       console.log('seconds:', seconds);
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+}); 
+player.addEventListener('timeupdate', getEventCurrentTime);
+
+function getEventCurrentTime(e) {
+    
+}
